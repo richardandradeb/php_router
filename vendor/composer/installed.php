776bec1593c26b4ca9ef5501ea7bab340bfbadc7
Richard <richardandrade@gmail.com>
2023-06-22 23:08:41 -0300

    fixes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'richard/php_router',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fe0339f01514a03e972245f5adffd3003e1c335b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'richard/php_router' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fe0339f01514a03e972245f5adffd3003e1c335b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
